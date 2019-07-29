f13dc6b8c3174c4bedb5978e0c749f4724594c08
Cormac Lawlor <armedcor@gmail.com>
2019-07-29 10:26:14 +0000

    enabled an add category button and wired up

--- app.py
+++ app.py
@@ -84,6 +84,17 @@ def delete_category(category_id):
     return redirect(url_for('get_categories'))    
 
 
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/new_category')
+def new_category():
+    return render_template('addcategory.html')    
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
